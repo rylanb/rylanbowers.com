51b8360d5bc9fcd4584a354f61ab405214f7c1cd
Rylan Bowers <rylanb@gmail.com>
2020-08-23 14:31:04 -0600

    Turning off minify css to test build fails

--- new_site/config.rb
+++ new_site/config.rb
@@ -23,7 +23,7 @@ set :haml, { format: :html5 }
 
 # Build-specific configuration
 configure :build do
-  activate :minify_css
+  # activate :minify_css
   activate :minify_javascript
   activate :asset_hash # Enable cache buster
 end
