01ede1ba7e1b9ea428b1fe6aef9109a8a6abec34
Rylan Bowers <rylanb@gmail.com>
2020-08-23 14:40:58 -0600

    Putting minify_css back. Clean-up

--- new_site/config.rb
+++ new_site/config.rb
@@ -1,19 +1,5 @@
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
 # Reload the browser automatically whenever files change
 activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 set :build_dir, 'build'
 set :css_dir, 'css'
@@ -23,7 +9,7 @@ set :haml, { format: :html5 }
 
 # Build-specific configuration
 configure :build do
-  # activate :minify_css
+  activate :minify_css
   activate :minify_javascript
   activate :asset_hash # Enable cache buster
 end
