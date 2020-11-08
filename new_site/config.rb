f2434dc023b2b4298f469266be085a96b8235386
Rylan Bowers <rylanb@gmail.com>
2020-11-08 16:33:42 -0700

    Try not minifying

--- new_site/config.rb
+++ new_site/config.rb
@@ -11,8 +11,8 @@ set :haml, { format: :html5 }
 
 # Build-specific configuration
 configure :build do
-  activate :minify_css
-  activate :minify_javascript
+  #activate :minify_css
+  #activate :minify_javascript
   # activate :asset_hash # Enable cache buster
   activate :gzip
 end
